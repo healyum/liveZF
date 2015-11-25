8509659a3fdc488c2066b3934caec098280593ca
BenjaminBermont <benji-du-51@hotmail.fr>
2015-11-25 16:16:53 +0100

    declaratif

--- module/MiniModule/Module.php
+++ module/MiniModule/Module.php
@@ -8,16 +8,6 @@ use Zend\Mvc\MvcEvent;
 use Zend\Mvc\Router\Http\Literal;
 use Zend\Mvc\Router\Http\TreeRouteStack;
 use Zend\View\Resolver\TemplateMapResolver;
-use Zend\Mvc\Controller\AbstractActionController;
-
-class IndexController extends AbstractActionController
-{
-	public function indexAction()
-	{
-		$nom = "tintin";
-		return array('nom' => $nom);
-	}
-}
 
 class Module implements BootstrapListenerInterface, ConfigProviderInterface
 {
@@ -31,35 +21,22 @@ class Module implements BootstrapListenerInterface, ConfigProviderInterface
         // $e->getTarget() renvoit Zend\MVC\Application
         $application = $e->getTarget();
         $sm = $application->getServiceManager();
-        $route = $sm->get('Router');
-        //$route = new TreeRouteStack();
-        $route->addRoute('home', Literal::factory(array(
-            'route' => '/',
-            'defaults' => array(
-                'controller' => 'MiniModule\Controller\Index',
-                'action' => 'index'
-                )
-            )
-        ));
-
-		$controller = $sm->get('ControllerManager');
-		$controller->setService('MiniModule\Controller\Index', new IndexController());
-	
-		/*
-		 $view = $sm->get('ViewManager');
-		 $resolv = new TemplateMapResolver(array(
-		 'error' => __DIR__.'/view/error.phtml',
-		 'layout/layout' => __DIR__.'/view/layout/layout.phtml',
-		 )
-		 );
-		 $view->getRenderer()->setResolver($resolv);
-		 */
 		
 		$event = $application->getEventManager();
 		$event->attach(MvcEvent::EVENT_DISPATCH_ERROR, function(MvcEvent $e) {
 			error_log($e->getError());
 			error_log($e->getControllerClass().' '.$e->getController());
 		});
-		
+    }
+    
+    public function getAutoloaderConfig()
+    {
+    	return array(
+    			'Zend\Loader\StandardAutoloader' => array(
+    					'namespaces' => array(
+    							__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+    					),
+    			),
+    	);
     }
 }
