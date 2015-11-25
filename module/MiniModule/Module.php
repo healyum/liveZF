81171c284f6e7aefb54e1de1c946cdf69bebf1b7
BenjaminBermont <benji-du-51@hotmail.fr>
2015-11-25 15:23:13 +0100

    Réécriture du code en mode déclaratif

--- module/MiniModule/Module.php
+++ module/MiniModule/Module.php
@@ -8,6 +8,16 @@ use Zend\Mvc\MvcEvent;
 use Zend\Mvc\Router\Http\Literal;
 use Zend\Mvc\Router\Http\TreeRouteStack;
 use Zend\View\Resolver\TemplateMapResolver;
+use Zend\Mvc\Controller\AbstractActionController;
+
+class IndexController extends AbstractActionController
+{
+	public function indexAction()
+	{
+		$nom = "tintin";
+		return array('nom' => $nom);
+	}
+}
 
 class Module implements BootstrapListenerInterface, ConfigProviderInterface
 {
@@ -22,30 +32,34 @@ class Module implements BootstrapListenerInterface, ConfigProviderInterface
         $application = $e->getTarget();
         $sm = $application->getServiceManager();
         $route = $sm->get('Router');
-        $route = new TreeRouteStack();
+        //$route = new TreeRouteStack();
         $route->addRoute('home', Literal::factory(array(
             'route' => '/',
             'defaults' => array(
-                'controller' => 'index',
+                'controller' => 'MiniModule\Controller\Index',
                 'action' => 'index'
                 )
             )
         ));
 
-        $event = $application->getEventManager();
-        $event->attach(MvcEvent::EVENT_DISPATCH_ERROR, function(MvcEvent $e) {
-            error_log($e->getError());
-            error_log($e->getControllerClass().' '.$e->getController());
-        });
-
-        /*
-        $view = $sm->get('ViewManager');
-        $resolv = new TemplateMapResolver(array(
-                'error' => __DIR__.'/view/error.phtml',
-                'layout/layout' => __DIR__.'/view/layout/layout.phtml',
-            )
-        );
-        $view->getRenderer()->setResolver($resolv);
-        */
-	}
+		$controller = $sm->get('ControllerManager');
+		$controller->setService('MiniModule\Controller\Index', new IndexController());
+	
+		/*
+		 $view = $sm->get('ViewManager');
+		 $resolv = new TemplateMapResolver(array(
+		 'error' => __DIR__.'/view/error.phtml',
+		 'layout/layout' => __DIR__.'/view/layout/layout.phtml',
+		 )
+		 );
+		 $view->getRenderer()->setResolver($resolv);
+		 */
+		
+		$event = $application->getEventManager();
+		$event->attach(MvcEvent::EVENT_DISPATCH_ERROR, function(MvcEvent $e) {
+			error_log($e->getError());
+			error_log($e->getControllerClass().' '.$e->getController());
+		});
+		
+    }
 }
--- module/MiniModule/config/module.config.php
+++ module/MiniModule/config/module.config.php
@@ -4,6 +4,7 @@ return array(
         'template_map' => array(
             'error' => __DIR__ . '/../view/error.phtml',
             'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
+        	'mini-module/index/index' => __DIR__ . '/../view/index/index.phtml',
         ),
     ),
 );
