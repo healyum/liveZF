62063adb2964cb7eadefe3b962d1aab39c3c1e1a
BenjaminBermont <benji-du-51@hotmail.fr>
2015-11-27 14:45:44 +0100

    formulaire ultra simple class form

--- module/MiniModule/src/MiniModule/Controller/IndexController.php
+++ module/MiniModule/src/MiniModule/Controller/IndexController.php
@@ -1,27 +1,58 @@
 <?php
 namespace MiniModule\Controller;
 
+use Zend\Form\Factory;
 use Zend\Mvc\Controller\AbstractActionController;
 
+/**
+ * Class IndexController
+ *
+ * @package MiniModule
+ */
 class IndexController extends AbstractActionController
 {
-	public function indexAction()
-	{
-		return array();
-	}
-	
-	public function formAction()
-	{
-		return array();
-	}
-	public function traiteAction()
-	{
-		return array( 'login' => $_GET['log'] );
-	}
-    
-    /*public function indexAction()
+    public function indexAction()
     {
-        $nom = 'tintin';
-        return array('no' => $nom);
-    }*/
+        return array();
+    }
+
+    public function formAction()
+    {
+        $configForm = array(
+            'elements' => array(
+                // la saisie du login (type text)
+                array(
+                    'spec' => array(
+                        'type' => 'Zend\Form\Element\Text',
+                        'name' => 'log',
+                        'attributes' => array(
+                            'size' => '20',
+                        ),
+                        'options' => array(
+                          'label' => 'Login : ',
+                        ),
+                    ),
+                ),
+                // le boutton de validation
+                array(
+                    'spec' => array(
+                        'type' => 'Zend\Form\Element\Submit',
+                        'name' => 'submit',
+                        'attributes' => array(
+                            'value' => 'Suite',
+                        ),
+                    ),
+                ),
+            ),
+        );
+
+        $factory = new Factory();
+        $form = $factory->createForm( $configForm );
+        return array( 'form' => $form );
+    }
+
+    public function traiteAction()
+    {
+        return array( 'login' => $_GET['log'] );
+    }
 }
